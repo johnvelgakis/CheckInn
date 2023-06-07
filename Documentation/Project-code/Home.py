7393367a8cb561c8bbc55971657dc02ef8117534
johnfelios <yiannisfelios@gmail.com>
2023-06-07 18:58:47 +0300

    add reservations and housekeeping in home menu

--- Documentation/Project-code/Home.py
+++ Documentation/Project-code/Home.py
@@ -2,7 +2,8 @@ import customtkinter
 from CheckIn import CheckIn
 from CheckOut import CheckOut
 from Requests import Requests
-
+from Reservations import Reservations
+from Housekeeping import Housekeeping
 
 customtkinter.set_appearance_mode("dark")
 customtkinter.set_default_color_theme("dark-blue")
@@ -20,14 +21,17 @@ class Home:
         button2 = customtkinter.CTkButton(master=self.frame, text="Check Out", command=self.go_to_check_out)
         button2.pack(pady=12, padx=10)
 
-        button3 = customtkinter.CTkButton(master=self.frame, text="Housekeeping")
+        button3 = customtkinter.CTkButton(master=self.frame, text="Housekeeping", command=self.go_to_housekeeping)
         button3.pack(pady=12, padx=10)
 
         button4 = customtkinter.CTkButton(master=self.frame, text="Requests", command=self.go_to_requests)
         button4.pack(pady=12, padx=10)
 
-        button5 = customtkinter.CTkButton(master=self.frame, text="Logout", command= self.Logout)
+        button5 = customtkinter.CTkButton(master=self.frame, text="Reservations", command=self.go_to_reservations)
         button5.pack(pady=12, padx=10)
+
+        button6 = customtkinter.CTkButton(master=self.frame, text="Logout", command= self.Logout)
+        button6.pack(pady=12, padx=10)
 
 
     def go_to_check_in(self):
@@ -40,8 +44,18 @@ class Home:
         self.root.destroy()
         check_in_window.root.mainloop()
 
+    def go_to_housekeeping(self):
+        check_in_window = Housekeeping()
+        self.root.destroy()
+        check_in_window.root.mainloop()
+
     def go_to_requests(self):
         check_in_window = Requests()
+        self.root.destroy()
+        check_in_window.root.mainloop()
+
+    def go_to_reservations(self):
+        check_in_window = Reservations()
         self.root.destroy()
         check_in_window.root.mainloop()
 
