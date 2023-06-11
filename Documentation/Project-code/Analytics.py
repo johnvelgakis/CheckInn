c7c45a1ac98c39afd4af9cc84a3a3eae61f7890c
Ioannis Velgakis <57132327+johnvelgakis@users.noreply.github.com>
2023-06-11 14:08:10 +0300

    analytics module update

--- Documentation/Project-code/Analytics.py
+++ Documentation/Project-code/Analytics.py
@@ -41,10 +41,53 @@ class Home:
         ##button that takes the user back to home window
         self.back_button = customtkinter.CTkButton(master=self.frame, text="Back", command=self.to_guest_menu, border_width=0) 
         self.back_button.pack(pady=12, padx=10)
-
+    
+     # Placeholder function to go back to the home window
     def to_guest_menu(self):
         self.root.destroy()
         from GuestMenu import GuestMenu    
         home_window = GuestMenu()
-        home_window.root.mainloop()   
+        home_window.root.mainloop()  
 
+    def occupancy_report(self):
+        # Placeholder function for the Occupancy report
+
+        # Connect to the database
+        conn = sqlite3.connect('database.db')
+        cursor = conn.cursor()
+
+        # Fetch the required information from the reservations table
+        cursor.execute("SELECT arrival_date, departure_date, num_people FROM reservations")
+        results = cursor.fetchall()
+
+        # Prepare the message to display in the pop-up window
+        message = "Occupancy Report:\n\n"
+        for arrival_date, departure_date, num_people in results:
+            message += f"Arrival Date: {arrival_date}\nDeparture Date: {departure_date}\nNumber of People: {num_people}\n\n"
+
+        # Show the message in a pop-up window
+        messagebox.showinfo("Occupancy Report", message)
+
+        # Close the database connection
+        cursor.close()
+        conn.close()
+
+    def budget_report(self):
+        # Placeholder function for the Budget report
+        return
+
+    def fun_budget_report(self):
+        # Placeholder function for the Functional Budget report
+        return
+
+    def guest_pref_report(self):
+        # Placeholder function for the Guest Preferences report
+        return
+
+    def top_bookers_report(self):
+        # Placeholder function for the Top Bookers report
+        return
+
+    def action_log_report(self):
+        # Placeholder function for the Action Log report
+        return
